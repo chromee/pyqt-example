8ca491123c342b25ca40f21d5a5f2e1d5ed64550
chromee <odk.mmmm@gmail.com>
2018-10-20 22:47:53 +0900

    real time graph

---
+++ graph_timer.py
@@ -0,0 +1,37 @@
+# Add path to library (just for examples; you do not need this)
+# import initExample
+
+
+from pyqtgraph.Qt import QtGui, QtCore
+import numpy as np
+import pyqtgraph as pg
+
+app = QtGui.QApplication([])
+
+win = pg.GraphicsWindow(title="Basic plotting examples")
+win.resize(1000, 600)
+win.setWindowTitle('pyqtgraph example: Plotting')
+
+interval = 0.0001  # milisec
+
+plot = win.addPlot(title="Updating plot")
+y = plot.plot(pen='y')
+x = 2*np.pi
+time = list(np.linspace(0, x, 1000))
+
+
+def update():
+    global y, time, x, interval
+    y.setData(time, np.sin(time))
+    x += 2*np.pi*interval*10
+    time.append(x)
+
+
+timer = QtCore.QTimer()
+timer.timeout.connect(update)
+timer.start(interval*1000)
+
+if __name__ == '__main__':
+    import sys
+    if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
+        QtGui.QApplication.instance().exec_()
